eb03fda04a31e15d5060dbe5298bd8c3862e10c5
Nahuel Krowicki <nahuelkrowicki@gmail.com>
2020-04-23 18:45:41 -0300

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,26 @@ module.exports = {
     title: config.siteTitle,
   },
   plugins: [
+    {
+      resolve: `gatsby-theme-localization`,
+      options: {
+        languages: ['en', 'es'],
+        namespaces: ['translation', 'about'],
+        localesDir: './src/locales',
+        allowIndex: false,
+        defaultLng: 'en',
+        i18next: {
+          // whatever you want to pass to react-i18next
+          fallbackLng: 'en',
+          debug: process.env.NODE_ENV !== 'production'
+        },
+        i18nPlugin: {
+          // whatever you want to pass to gatsby-plugin-i18n
+          langKeyDefault: 'en',
+          useLangKeyLayout: false
+        }
+      }
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
