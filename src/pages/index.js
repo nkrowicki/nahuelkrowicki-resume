482b0531a35531dc42e823cbc7788c6cf4e23f74
Nahuel Krowicki <nahuelkrowicki@gmail.com>
2020-04-24 17:53:40 -0300

    Modified index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
-import IndexPage from '../components/pages';
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+    <Layout>
+        <div className="container-fluid p-0">
+            <section
+                className="resume-section p-3 p-lg-5 d-flex align-items-center"
+                id="about"
+            >
+                <header className="major">
+                    <h2>
+                        H E L L O !
+            <br />
+            H O L A !
+          </h2>
+                </header>
+            </section>
+        </div>
+    </Layout>
+);
 
 export default IndexPage;
